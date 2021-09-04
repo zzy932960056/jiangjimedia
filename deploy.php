9212347bfff893dadc7d67708f1c87aacc4e92c9
zhaozeyu <18645124160@163.com>
2021-09-04 13:33:08 +0800

    deploy

--- deploy.php
+++ deploy.php
@@ -82,7 +82,7 @@ after('deploy', 'success');
 
 // Configuration
 set('writable_use_sudo', false);
-set('repository', 'git@git.coding.net:zzy932960056/jiangjimedia.git');
+set('repository', 'git@github.com:zzy932960056/jiangjimedia.git');
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
